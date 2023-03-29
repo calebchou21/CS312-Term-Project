2482cd156f8ba5ac498187774469c4827831a7ca
Jake Stinson <jakestinson44@icloud.com>
2023-03-29 11:30:07 -0600

    I changed my about me bio

--- fuel/app/views/milestone/about.php
+++ fuel/app/views/milestone/about.php
@@ -27,13 +27,13 @@
         <div class="teamimg"><img src="" alt=""></div>
         <div class="person">
             <h2>Jake Stinson</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Doloribus quos cupiditate quam officiis? Modi tempore mai
-                ores id accusantium et aspernatur provident optio facilis 
-                commodi ea incidunt corporis dignissimos ex alias dolorem,
-                 veritatis dolorum laborum error quas dolore dolor? Totam e
-                 veniet aut fuga rem illo laboriosam nesciunt numquam nihil
-                  aliquam quaerat.
+            <p>Hello! I am a 2nd year computer science major at Colorado State University. 
+                I am a general concentration comp sci major, who is still figuring out exactly
+                what I want to do within the field. Outside of school, I spend my time with friends,
+                rock climbing, running, playing video games, watching movies, or spending time volunteering
+                for a program called Young Life. I also work part-time at Starbucks. A fun fact about me is
+                that I have signed up for the show Survivor two times and will sign up until I get on the show!
+                
             </p>
         </div>
         <div class="teamimg"><img src="" alt=""></div>
