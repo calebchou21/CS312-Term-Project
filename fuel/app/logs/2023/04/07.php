4b133fa1c0ed93040299eccb438dff5b377bdbf8
Jake Stinson <jakestinson44@icloud.com>
2023-04-26 11:10:17 -0600

    yes

---
+++ fuel/app/logs/2023/04/07.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2023-04-07 13:12:09 --> Error - Could not find asset: bootstrap.css in /s/parsons/n/under/jake44/local_html/m1/fuel/core/classes/asset/instance.php on line 413
