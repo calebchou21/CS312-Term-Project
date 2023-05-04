a54d0d06f022b2d60e282191de317a8e273781ac
Caleb Chou <calebrc@colostate.edu>
2023-05-04 14:57:02 -0600

    db setup

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -23,8 +23,8 @@ return array(
 	'default' => array(
 		'connection' => array(
 			'dsn'      => 'mysql:host=localhost;dbname=fuel_dev',
-			'username' => 'root',
-			'password' => 'root',
+			'username' => 'calebrc',
+			'password' => '835407623',
 		),
 	),
 );
