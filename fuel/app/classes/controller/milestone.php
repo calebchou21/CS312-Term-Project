869f9b8881f1f69d3a017a508f3c9789ee67c9a4
Caleb Chou <calebrc@colostate.edu>
2023-05-05 14:03:10 -0600

    created model and updated controller

--- fuel/app/classes/controller/milestone.php
+++ fuel/app/classes/controller/milestone.php
@@ -60,10 +60,31 @@ class Controller_Milestone extends Controller_Template {
        $this->template->content = View::forge('milestone/printView', $data);
    }
 // I Think we need to do something here to handle the post from colorCoordinator so the add.php can run its course."
-   public function post_add(){
-     $data = array();
-     $this->template->content = View::forge('milestone/add', $data);
-     action_colorCoordinator();
+   public function post_index(){
+
+     //IDK IF THE COLORNAME AND HEXVAL POSTS ARE RIGHT
+     if (isset($_POST['add']) && isset($_POST['colorName']) && isset($_POST['hexVal'])) {
+          m2Model::add_color($_POST['colorName'], $_POST['hexVal']);
+     }
+
+
+     //Copied kind from todo lab. I think we can post all checked colors and then delete them?
+     if (isset($_POST['delete']) && isset($_POST['checked'])) {
+          $checked_colors = array();
+          foreach ( $_POST['colorcheck'] as $id ) {
+              $checked_colors[] = $id;
+          }
+
+          //we dont have this function in the model yet
+          m2Model::delete_color($checked_colors);
+     }
+
+     $data = array(
+          'colors' => m2Model::read_colors(),
+          'todo_count' => m2Model::color_count()
+     );
+     //I dont kow what to forge here
+     //return Response::forge(View::forge('tododbviews/todomaincontent', $data));
    }
 
    
